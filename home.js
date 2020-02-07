5fe5964f8ebbc6eea20f52aa839a6c399abbc856
Una Kravets <unakravets@unakravets-macbookpro2.roam.corp.google.com>
2020-02-07 16:12:51 -0500

    fix bug with jumping on clipboard copy

--- home.js
+++ home.js
@@ -735,6 +735,9 @@ document.querySelector('.copyBtn').addEventListener('click', (e) => {
   input.value = text
   input.style.display="fixed"  // avoid scrolling to bottom and showing input
   input.style.opacity="0"  // visually hide
+  input.style.position="absolute"
+  input.style.padding="0"
+  input.style.width="0"
 
   // Select the text
   input.focus()
